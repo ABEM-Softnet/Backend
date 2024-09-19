4e90be105555a1db7fe0f0b0dab8a08ed7ca5f3f
Elisha <elishabu28@gmail.com>
2024-09-20 02:12:51 +0300

    branch model updated

--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -16,18 +16,22 @@ class Branch extends Model
         // Add any fillable fields here
     ];
 
-    public function school(): BelongsTo
-    {
+    public function school(){
         return $this->belongsTo(School::class);
     }
-
     public function revenues(): HasMany
     {
         return $this->hasMany(Revenue::class);
     }
-
     public function expenses()
     {
         return $this->hasMany(Expense::class);
+    }
+
+    public function gradeDivisions(){
+        return $this->hasMany(GradeDivision::class);
+    }
+    public function schoolLeaders(){
+        return $this->hasMany(SchoolLeader::class);
     }
 }
