bb30c58a68843fb94a5b2ca922b89bb3f64c2d1f
Elisha <elishabu28@gmail.com>
2024-09-20 02:15:05 +0300

    api route updated

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,39 @@ use App\Http\Controllers\Api\v1\FinancialController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::get('/students', function () {
+    return Student::with('branch')->get();
+});
+
+
+Route::post('/admin-register', [SessionController::class, 'adminRegister']);
+
+Route::post('/login', [SessionController::class, 'login']);
+
+Route::post('/teacher-register',[SessionController::class,'teacherRegister']);
+
+Route::post('forgot-password', [NewPasswordController::class, 'forgotPassword']);
+
+Route::post('reset-password', [NewPasswordController::class, 'reset']);
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::delete('/user-delete', [SessionController::class, 'destroy']);
+    Route::patch('/user-update', [SessionController::class, 'update']);
+    Route::post('/logout', [SessionController::class,'logout']);
+    Route::get('/verified-middleware', function(){
+        return response()->json([
+            'message'=> 'The email account is already confirmed now you are able to see this message...',
+        ], 201);
+    });
+
+    Route::post('email/verification-notification', [EmailVerificationController::class, 'sendVerificationEmail']);
+    Route::post('verify-email/{id}/{hash}', [EmailVerificationController::class, 'verify'])->name('verification.verify');
+
+
+});
+
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('/test', function () {
